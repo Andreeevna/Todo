a16cbf0675934bee479f501d89a38e8af8d2bab0
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-05-15 16:37:29 +0300

    style: stylized header markup

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,23 @@
+import cn from 'classnames'
+
 import dataImg from './../../assets/icon/date.svg'
 import searchImg from './../../assets/icon/search.svg'
 import ProfileImg from './../../assets/img/Profile.png'
 
+import s from './../layout/Layout.module.css'
 import styles from './Header.module.css'
 
 const Header: React.FC = () => {
 	return (
-		<div className={styles.header}>
-			<h2 className={styles.header__title}>Todolist</h2>
-			<div className={styles.header__item}>
-				<img src={searchImg} />
-				<img src={dataImg} />
-				<span>19 May 2022</span>
-				<img className={styles.header__img} src={ProfileImg} />
+		<div className={styles.wrapper__header}>
+			<div className={cn(styles.header, s.wrapper)}>
+				<h1 className={styles.header__title}>Todolist</h1>
+				<div className={styles.header__item}>
+					<img src={searchImg} />
+					<img src={dataImg} />
+					<span>19 May 2022</span>
+					<img className={styles.header__img} src={ProfileImg} />
+				</div>
 			</div>
 		</div>
 	)
--- src/components/header/dist/Header.js
+++ src/components/header/dist/Header.js
@@ -1,16 +1,19 @@
 "use strict";
 exports.__esModule = true;
+var classnames_1 = require("classnames");
 var date_svg_1 = require("./../../assets/icon/date.svg");
 var search_svg_1 = require("./../../assets/icon/search.svg");
 var Profile_png_1 = require("./../../assets/img/Profile.png");
+var Layout_module_css_1 = require("./../layout/Layout.module.css");
 var Header_module_css_1 = require("./Header.module.css");
 var Header = function () {
-    return (React.createElement("div", { className: Header_module_css_1["default"].header },
-        React.createElement("h2", { className: Header_module_css_1["default"].header__title }, "Todolist"),
-        React.createElement("div", { className: Header_module_css_1["default"].header__item },
-            React.createElement("img", { src: search_svg_1["default"] }),
-            React.createElement("img", { src: date_svg_1["default"] }),
-            React.createElement("span", null, "19 May 2022"),
-            React.createElement("img", { className: Header_module_css_1["default"].header__img, src: Profile_png_1["default"] }))));
+    return (React.createElement("div", { className: Header_module_css_1["default"].wrapper__header },
+        React.createElement("div", { className: classnames_1["default"](Header_module_css_1["default"].header, Layout_module_css_1["default"].wrapper) },
+            React.createElement("h1", { className: Header_module_css_1["default"].header__title }, "Todolist"),
+            React.createElement("div", { className: Header_module_css_1["default"].header__item },
+                React.createElement("img", { src: search_svg_1["default"] }),
+                React.createElement("img", { src: date_svg_1["default"] }),
+                React.createElement("span", null, "19 May 2022"),
+                React.createElement("img", { className: Header_module_css_1["default"].header__img, src: Profile_png_1["default"] })))));
 };
 exports["default"] = Header;
